6c8ab68b951c3a0a9209299256f08343b79399c4
ReallyNguyen <jnguyen188@my.bcit.ca>
2023-05-31 23:53:31 -0700

    normal

--- pages/_document.js
+++ pages/_document.js
@@ -1,19 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="preload" href="/styles/Home.module.css" as="style" />
-          <link rel="stylesheet" href="/styles/Home.module.css" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
-
