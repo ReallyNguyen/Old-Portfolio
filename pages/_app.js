58dc5bdb07aa4f2a8355a9a24af2be506818b2a0
ReallyNguyen <jnguyen188@my.bcit.ca>
2023-06-01 17:28:04 -0700

    test page transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import Head from 'next/head'
-import { AnimatePresence } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
@@ -14,9 +14,29 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <AnimatePresence mode="wait">
-        <Component key={router.asPath} {...pageProps} />
+        <motion.div
+          key={router.route}
+          initial="initialState"
+          animate="animateState"
+          exit="exitState"
+          transition={{ duration: 0.75 }}
+          variants={{
+            initialState: {
+              opacity: 0,
+              clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
+            },
+            animateState: {
+              opacity: 1,
+              clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)"
+            },
+            exitState: {
+              clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)"
+            },
+          }}
+        >
+          <Component key={router.asPath} {...pageProps} />
+        </motion.div>
       </AnimatePresence>
-
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -16,11 +16,10 @@ export default function Home() {
 
   return (
     <>
-      <Transition />
+
       <m.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
         transition={{ duration: 0.75, delay: 0.6, ease: "easeInOut" }}
       >
         <main className={styles.main}>
